13cd9c7d1d39a0d9f0ca54aed50b3d525c97b6db
HoussamTachnout <houssam.tachnout@usmba.ac.ma>
2022-10-28 11:52:20 +0100

    Suite 1

--- Program.cs
+++ Program.cs
@@ -9,9 +9,24 @@ namespace Exercice2
     class Program
     {
         static void Main(string[] args)
-        {
-            int []tab = new int[] {2,3,5,6,7};
-            //
+        {   
+            int n,i;
+            int[] tab = new int[] { 2, 3, 5, 6, 7};
+            Console.Write("Enter un nombre : ");
+            n = int.Parse(Console.ReadLine());
+            for(i = 4; i > 0; i--)
+            {
+                if (tab[i-1] > n)
+                {
+                    tab[i] = tab[i - 1];
+                    tab[i - 1] = n;
+                }
+            }
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("\t" + tab[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
